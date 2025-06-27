7f046aba10f8d99f5e2d8601d1f5412fd121c079
Hadar gruper <gruper9@gmail.com>
2025-06-27 17:28:07 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: '/comet-guild-gateway/',
+  base: '/comet-guild-gateway/',
   server: {
     host: "::",
     port: 8080,
